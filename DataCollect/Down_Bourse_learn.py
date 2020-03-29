9cae22ccb66cdb5f0da2a137be6dcc6f0e0ee431
@adarban94 <adarban94@gmail.com>
2020-03-29 14:31:43 +0430

    continue of video collecting and del the workflow

--- DataCollect/Down_Bourse_learn.py
+++ DataCollect/Down_Bourse_learn.py
@@ -2,8 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 
 # Some codes for Agah Bashgah leaning bourse videos
+# Main web address
 
-url = "https://bashgah.com/blog/دانلود-رایگان-فیلم-آموزش-بورس/"   # Main web address
+url = "https://bashgah.com/blog/دانلود-رایگان-فیلم-آموزش-بورس/"
 
 Bashgah = requests.get(url)  # create HTTP response object
 
@@ -16,11 +17,24 @@ Bashgah_soup = BeautifulSoup(Bashgah.content, "html.parser")
 Bashgah_soup_class = Bashgah_soup.find_all(
     'div', attrs={'class': 'wpb_wrapper'})
 
-print(type(Bashgah_soup_class))
 for items in Bashgah_soup_class:
     if items.ul is not None:
         titles = items.p.text.strip()
         subtitles = items.ul.find_all('li')
         print(titles + "\n --------- \n ")
         for sub in subtitles:
-            print(sub.a.text + ": " + sub.a["href"] + '\n')
+            subpage = sub.a["href"]
+            subtext = sub.a.text
+
+            subpage_url = requests.get(subpage)
+            subpage_soup = BeautifulSoup(subpage_url.content, "html.parser")
+            subpage_soup_class = subpage_soup.find(
+                'div', attrs={'class': 'px-30 container content'})
+            if subpage_soup_class is not None:
+                check_videos = subpage_soup_class.find_all(
+                    'a', attrs={'rel': "noopener noreferrer"})
+                for videos_ls in check_videos:
+                    if videos_ls is not None and ".mp4" in videos_ls["href"]:
+
+                        video_href = videos_ls["href"]
+                        print(subtext + ": " + video_href + '\n')
