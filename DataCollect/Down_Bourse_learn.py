62a4bb97cbd74c176f0d3449316b9fb9501c763d
@adarban94 <adarban94@gmail.com>
2020-03-29 11:34:23 +0430

    First collecting from bourse videos

--- DataCollect/Down_Bourse_learn.py
+++ DataCollect/Down_Bourse_learn.py
@@ -1,10 +1,9 @@
 import requests
-# import re
 from bs4 import BeautifulSoup
 
 # Some codes for Agah Bashgah leaning bourse videos
 
-url = "https://bashgah.com/blog/دانلود-رایگان-فیلم-آموزش-بورس/"     # Main web address
+url = "https://bashgah.com/blog/دانلود-رایگان-فیلم-آموزش-بورس/"   # Main web address
 
 Bashgah = requests.get(url)  # create HTTP response object
 
@@ -12,14 +11,16 @@ Bashgah = requests.get(url)  # create HTTP response object
 # the HTTP response in a response object called Bashgah
 
 # Parse the html webpage
-Bashgah_soup = BeautifulSoup(Bashgah.text, "html.parser")
+Bashgah_soup = BeautifulSoup(Bashgah.content, "html.parser")
 
 Bashgah_soup_class = Bashgah_soup.find_all(
     'div', attrs={'class': 'wpb_wrapper'})
 
+print(type(Bashgah_soup_class))
 for items in Bashgah_soup_class:
-    titles = items.p.text.strip()
-    subtitles = titles.find_all('a').text.strip()
-    print(titles + "\n --------- \n \n")
-    for sub in subtitles:
-        print(sub + ': ' + sub['href'] + '\n')
+    if items.ul is not None:
+        titles = items.p.text.strip()
+        subtitles = items.ul.find_all('li')
+        print(titles + "\n --------- \n ")
+        for sub in subtitles:
+            print(sub.a.text + ": " + sub.a["href"] + '\n')
