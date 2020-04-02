3014105e0e7e3958d813017411fe9095e487c2d6
@adarban94 <adarban94@gmail.com>
2020-04-02 18:25:52 +0430

    Change the main of code and update gitignore and del the trash

--- DataCollect/Down_Bourse_learn.py
+++ DataCollect/Down_Bourse_learn.py
@@ -15,26 +15,15 @@ Bashgah = requests.get(url)  # create HTTP response object
 Bashgah_soup = BeautifulSoup(Bashgah.content, "html.parser")
 
 Bashgah_soup_class = Bashgah_soup.find_all(
-    'div', attrs={'class': 'wpb_wrapper'})
+    'div', attrs={'class': 'wpb_text_column'})
 
-for items in Bashgah_soup_class:
-    if items.ul is not None:
-        titles = items.p.text.strip()
-        subtitles = items.ul.find_all('li')
-        print(titles + "\n --------- \n ")
-        for sub in subtitles:
-            subpage = sub.a["href"]
-            subtext = sub.a.text
+# creating txt for storage collected data
 
-            subpage_url = requests.get(subpage)
-            subpage_soup = BeautifulSoup(subpage_url.content, "html.parser")
-            subpage_soup_class = subpage_soup.find(
-                'div', attrs={'class': 'px-30 container content'})
-            if subpage_soup_class is not None:
-                check_videos = subpage_soup_class.find_all(
-                    'a', attrs={'rel': "noopener noreferrer"})
-                for videos_ls in check_videos:
-                    if videos_ls is not None and ".mp4" in videos_ls["href"]:
+f = open("draft.txt", "w", encoding="utf-8")
 
-                        video_href = videos_ls["href"]
-                        print(subtext + ": " + video_href + '\n')
+for headlines in Bashgah_soup_class:
+    subheadlines = headlines.find_all('div', attrs={'class': 'wpb_wrapper'})
+    for subhead in subheadlines:
+        if len(subhead.text) != 1:
+            f.write(subhead.p.text.strip())
+f.close()
