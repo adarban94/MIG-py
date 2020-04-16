7ab05f97484619f3775cdaa3edeaa11dac8b2598
@adarban94 <adarban94@gmail.com>
2020-04-16 22:16:06 +0430

    every thing must be change

---
+++ TeleBot/Hamidconcated.py
@@ -0,0 +1,74 @@
+import json
+from time import sleep
+from datetime import datetime, timedelta
+
+# Open price databases to read
+with open(r"E:\Business\MIG\Codes\MIG-py\TeleBot\new_json_data.json", 'r', encoding="utf-8") as f1:
+    filedata1 = json.loads(f1.read())
+
+# Open the senario database read
+with open(r"E:\Business\MIG\Codes\MIG-py\TeleBot\manage_senario.json", 'r', encoding="utf-8") as f2:
+    filedata2 = json.loads(f2.read())
+
+# open the prices files
+IDlist = filedata1["ID"]
+abshodelist = filedata1["ABSHODE FARDAII"]
+timelist = filedata1["TIME"]
+
+# convert time string to time object
+timeobject = len(timelist) * [0]
+for id, t in enumerate(timelist):
+    timeobject[id] = datetime.strptime(t, '%Y-%m-%d %H:%M:%S')
+
+senario_namelist = filedata2["senario_name"]
+
+# notice that the type of elements in this list is string! not INTEGER
+senario_Plist = [float(i) for i in filedata2["senario_P"]]
+
+# notice that the type of elements in this list is string! not INTEGER
+senario_Tlist = [float(i) for i in filedata2["senario_T"]]
+
+# sharps = {}
+# for name in senario_namelist:
+#    sharps[name] =
+
+# Initial varialbes for prices and
+idT = len(senario_Tlist) * [0]
+prices = idT[:]
+maxprices = idT[:]
+minprices = idT[:]
+spanprices = idT[:]
+lastprices = idT[:]
+
+# determine the time id from time span for all senario_T and prices
+for i, item in enumerate(idT):
+    for id, tobj in enumerate(timeobject[::-1], start=1):
+
+        timediff = timeobject[-1] - tobj
+        timediff2 = timeobject[-1] - timeobject[-2]
+        if timediff.total_seconds() <= timedelta(seconds=senario_Tlist[i]).total_seconds():
+            idT[i] = -id-1
+        elif timediff2.total_seconds() > timedelta(seconds=senario_Tlist[i]).total_seconds():
+            idT[i] = -2
+    prices[i] = abshodelist[idT[i]:]
+    maxprices[i] = max(prices[i])
+    minprices[i] = min(prices[i])
+    spanprices[i] = float(maxprices[i].replace(',', '')) - \
+        float(minprices[i].replace(',', ''))
+    lastprices[i] = float(abshodelist[-1].replace(',', ''))
+
+    if spanprices[i] >= senario_Plist[i]:
+        # maxprices[i] must insert into json file
+        # minprices[i] must insert into json file
+        # lastprices[i] must insert into json file
+
+        if lastprices[i] - minprices[i] > pricemotivation:
+            print("آلارم شماره: %d \
+               از سناریو نوع: %s معتبر می باشد \
+                \n بیشترین قیمت این شارپ: %d \
+                \n کمترین قیمت این شارپ: %d \
+                \n آخرین قیمت اعلام شده: %d \
+                \n %s" % (Sharpdic["ID"], Sharpdic["senario"][0], maxPrice, minPrice, lastPrice, datetime.now()))
+        else:
+            print("آلارم شماره: %d \
+            از نوع سناریو: %s منقضی شد \n %s " % (sharpdic["ID"], sharpdic["senario"][0], datetime.now()))
--- TeleBot/Senario.py
+++ TeleBot/Senario.py
@@ -1,17 +1,103 @@
 import json
-import re
+from time import sleep
+from datetime import datetime
 
 
-# Declare the senarios fun
+def Pricedata(status):
+    if status == "read":
+        # Open price databases to read
+        with open(r"E:\Business\MIG\Codes\MIG-py\TeleBot\new_json_data.json", 'r', encoding="utf-8") as f1:
+            filedata1 = json.loads(f1.read())
+        return (filedata1, f1)
+    elif status == "write":
+        pass
 
-def senarios(Name, T, P, PM):
 
-    with open(r"E:\Business\MIG\Codes\MIG-py\TeleBot\new_json_data.json", 'r', encoding="utf-8") as f:
-        file = f.read()
-        filedata1 = json.loads(file)
-    #  with open("manage_senario.json", 'r', encoding="utf-8") as f:
-    #    filedata2 = json.loads(f.read())
+def Senariodata(status):
+    if status == "read":
+        # Open the senario database read
+        with open(r"E:\Business\MIG\Codes\MIG-py\TeleBot\manage_senario.json", 'r', encoding="utf-8") as f2:
+            filedata2 = json.loads(f2.read())
+        return (filedata2, f2)
+    elif status == "write":
+        pass
+
+
+def Sharpdata(status):
+    if status == "read":
+        # Open the sharp database read
+        with open(r"E:\Business\MIG\Codes\MIG-py\TeleBot\manage_sharp.json", 'r', encoding="utf-8") as f3:
+            filedata3 = json.loads(f3.read())
+        return (filedata3, f3)
+    elif status == "write":
+        # Open the sharp database write
+        with open(r"E:\Business\MIG\Codes\MIG-py\TeleBot\manage_sharp.json", 'w', encoding="utf-8") as f3:
+            filedata3 = json.loads(f3.read())
+        return (filedata3, f3)
+
+# Declare the senarios function
+
+
+def senarios(Name, T, P):
+
+    # call the data and files
+    (filedata1, f1) = Pricedata("read")
+    (filedata2, f2) = Senariodata("read")
+
+    # Check the size of time span to number of records in DataBase
+    # or senario name isn't a string object
     file_len = len(filedata1["ID"])
+    if T > file_len:
+        raise ValueError(
+            'Number of required records is more than the file size')
+    elif type(Name) != str:
+        raise Exception('Senario name must be string')
+
+    # Calculate max, min and desired price span from database
+    prices = filedata1["ABSHODE FARDAII"][-T:]
+    maxPrice = max(prices)
+    minPrice = min(prices)
+    SpanPrice = float(maxPrice.replace(',', '')) - \
+        float(minPrice.replace(',', ''))
+
+    # check the senario if recognize the sharp
+    if SpanPrice >= P:
+        f1.close()
+        f2.close()
+        return (minPrice, maxPrice)
+    else:
+        pass
 
 
-senarios(1, 1, 2, 3)
+def sharp(Name, T, P, Mov, tdelay=5):
+    i = 1
+    (minPrice, maxPrice) = senarios(Name, T, P)
+    sharpdata = {}
+    sharpdic = {"ID": [], "maxPrice": [], "minPrice": [], "lastPrice": []}
+    sharpdata[Name] = sharpdic
+    (filedata1, f1) = Pricedata("read")
+    lastPrice = filedata1["ABSHODE FARDAII"][-1]
+    f1.close()
+    # Check the last price to get motivation price
+    while lastPrice - minPrice < Mov:
+
+        sleep(tdelay*60)  # Wait 5 min to get new value for the last price
+        (filedata1, f1) = Pricedata("read")
+        lastPrice = filedata1["ABSHODE FARDAII"][-1]
+        f1.close()
+        print("آلارم شماره: %d \
+            از نوع سناریو: %s منقضی شد \n %s " % (sharpdic["ID"], sharpdic["senario"][0], datetime.now()))
+
+    else:
+
+        print("آلارم شماره: %d \
+               از سناریو نوع: %s معتبر می باشد \
+                \n بیشترین قیمت این شارپ: %d \
+                \n کمترین قیمت این شارپ: %d \
+                \n آخرین قیمت اعلام شده: %d \
+                \n %s" % (Sharpdic["ID"], Sharpdic["senario"][0], maxPrice, minPrice, lastPrice, datetime.now()))
+
+
+print(senarios("Ali", 20, 2000))
+print("\n")
+print(sharp())
